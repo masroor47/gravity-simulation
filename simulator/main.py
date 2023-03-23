c44cda2c0014d0ec77e267f513c1e25b375eafd7
Masroor Khonkhodzhaev <masroor47@icloud.com>
2023-03-22 21:08:57 -0400

    add gravity function, hardcoded three body case

--- simulator/main.py
+++ simulator/main.py
@@ -1,4 +1,5 @@
 import pygame
+import numpy as np
 
 from Body import Body
 
@@ -8,6 +9,29 @@ pygame.init()
 win = pygame.display.set_mode((600, 600))
 pygame.display.set_caption("Gravity Simulator")
 
+# G_const = 
+
+
+def gravity(one: Body, two: Body):
+    
+    distance = np.subtract(one.position, two.position)
+
+    distance_mag =  np.linalg.norm(distance)
+
+    direction_unit = distance / distance_mag
+
+    # print(f"unit: {distance / distance_mag}")
+
+    force = one.mass * two.mass / (distance_mag ** 2)
+
+    return force * direction_unit
+
+
+def accel_due_gravity(gravity, mass: float):
+
+    return np.array(gravity) / mass
+
+    
 
 
 
@@ -15,17 +39,24 @@ pygame.display.set_caption("Gravity Simulator")
 
 def main():
 
-    x0, y0 = 200, 200
-    vx0, vy0 = 3, 0
-    ax0, ay0 = -0.1, 0
+    x0, y0 = 300, 200
+    vx0, vy0 = -2, 0
+    ax0, ay0 = 0, 0
 
 
-    moon = Body(10, (x0, y0), (vx0, vy0), (ax0, ay0))
+    moon = Body(400, (x0, y0), (vx0, vy0), (ax0, ay0))
+
+    earth = Body(400, (200, 373.20508), (1, 1.720508), (0, 0))
+
+    mars = Body(400, (400, 373.20508), (1, -1.720508), (0, 0))
+
+
+
 
     run = True
 
     while run:
-        pygame.time.delay(100)
+        pygame.time.delay(30)
 
         for event in pygame.event.get():
 
@@ -35,11 +66,43 @@ def main():
 
         win.fill((0, 0, 0))
 
+        moon_earth_gravity = gravity(moon, earth)
+
+        moon_mars_gravity = gravity(moon, mars)
+
+        mars_earth_gravity = gravity(mars, earth)
+
+        moon_acceleraton = accel_due_gravity(-moon_earth_gravity, moon.mass) + accel_due_gravity(-moon_mars_gravity, moon.mass)
+        earth_acceleration = accel_due_gravity(moon_earth_gravity, earth.mass) + accel_due_gravity(mars_earth_gravity, earth.mass)
+        mars_acceleration = accel_due_gravity(moon_mars_gravity, mars.mass) + accel_due_gravity(-mars_earth_gravity, mars.mass)
+
+
+        moon.set_acceleration(moon_acceleraton)
+        earth.set_acceleration(earth_acceleration)
+        mars.set_acceleration(mars_acceleration)
+
+        earth.update_velocity()
+        earth.update_position()
+
+        moon.update_velocity()
         moon.update_position()
 
-        x, y = moon.get_position()
+        mars.update_velocity()
+        mars.update_position()
 
-        pygame.draw.circle(win, (113, 113, 113), (x, y), 10)
+        # x, y = 
+
+        # ex, ey = 
+
+        # print(gravity(moon, earth))
+
+
+
+        pygame.draw.circle(win, (113, 113, 113), moon.get_position(), 10)
+
+        pygame.draw.circle(win, (113, 200, 113), earth.get_position(), 20)
+
+        pygame.draw.circle(win, (200, 100, 100), mars.get_position(), 15)
 
         pygame.display.update()
 
